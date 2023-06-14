9e026f028d49421949ab74991488ae1bf6acc6b9
Raadius <raadius@yandex.ru>
2023-06-14 14:13:49 +0300

    fixed bugs

--- src/components/api-services/api-service.js
+++ src/components/api-services/api-service.js
@@ -11,13 +11,10 @@ export default class AviasalesService {
   }
 
   async getTickets() {
-    try {
-      const result = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${this._searchId}`).then(
-        (result) => result.json()
-      );
-      return result;
-    } catch (error) {
-      console.log(error);
+    const result = await fetch(`https://aviasales-test-api.kata.academy/tickets?searchId=${this._searchId}`);
+    if (!result.ok) {
+      throw new Error('Код ошибки: ' + result.status);
     }
+    return result.json();
   }
 }
